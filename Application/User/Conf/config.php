c586d6b09a7d79021299d6ccde35a64da03c3e46
viplaopan <531357117@qq.com>
2016-08-23 05:50:28 +0800

    3

--- Application/User/Conf/config.php
+++ Application/User/Conf/config.php
@@ -15,5 +15,5 @@
 define('UC_APP_ID', 1); //应用ID
 define('UC_API_TYPE', 'Model'); //可选值 Model / Service
 define('UC_AUTH_KEY', '.>|xjpz4^!wI0<C-Oh"86XtT3}m&c~B]9Poek,({'); //加密KEY
-define('UC_DB_DSN', 'mysql://root:d41b5a597e@127.0.0.1:3306/lqbuluo'); // 数据库连接，使用Model方式调用API必须配置此项
+define('UC_DB_DSN', 'mysql://root:Pa0d31f219@127.0.0.1:3306/zhouAd'); // 数据库连接，使用Model方式调用API必须配置此项
 define('UC_TABLE_PREFIX', 'onethink_'); // 数据表前缀，使用Model方式调用API必须配置此项
