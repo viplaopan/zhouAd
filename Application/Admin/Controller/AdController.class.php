7e8fef024ea19c8f7866d2e58757df9082478f88
viplaopan <531357117@qq.com>
2016-08-23 06:31:58 +0800

    3

--- Application/Admin/Controller/AdController.class.php
+++ Application/Admin/Controller/AdController.class.php
@@ -19,8 +19,9 @@ class AdController extends AdminController
 	//菜单列表
     public function index($page = 1, $r = 20)
     {
-    	$ip = getIPaddress();
-    	dump(taobaoIP($ip));
+    	import('ORG.Net.IpLocation');// 导入IpLocation类
+$Ip = new IpLocation('UTFWry.dat'); // 实例化类 参数表示IP地址库文件
+$area = $Ip->getlocation('203.34.5.66'); // 获取某个IP地址所在的位置
     	$adModel = D('Ad');  
 		$map['status'] = 1;
         $lists = $adModel->where($map)->page($page, $r)->order('id desc')->select();
