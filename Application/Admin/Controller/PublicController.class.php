5429fc9656d2186781341a7ec1dae9dc75af0e1a
viplaopan <531357117@qq.com>
2016-08-23 23:15:41 +0800

    2

--- Application/Admin/Controller/PublicController.class.php
+++ Application/Admin/Controller/PublicController.class.php
@@ -31,7 +31,7 @@ class PublicController extends \Think\Controller {
 	    echo 'province = remote_ip_info["province"];';
 	 	echo 'city = remote_ip_info["city"];';
 			echo 'if(' .$jsstr. '){';
-				echo 'document.writeln("<div class=\'\'><img src=\'http://ad.1bea.com/Uploads/Ad/' . $info['image'] . '\'></div>")';
+				echo 'document.writeln("<div class=\'\'><a href=\'' .$info['url']. ''\'><img src=\'http://ad.1bea.com/Uploads/Ad/' . $info['image'] . '\'></a></div>")';
 			echo '}';
 	    echo '}) ;';
 	}
